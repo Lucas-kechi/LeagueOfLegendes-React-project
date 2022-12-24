2b51fe0c496d7a147e6c633bf6c8afe017bc294a
A81435269g <lukaskechi7@gmail.com>
2022-12-24 12:52:20 -0300

    Moving fetch and Add filter Buttns css logic

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import './style.scss'
+import { useState } from 'react';
 
 export function Header() {
     const filterButtonsLogic = (btnId) => {
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,13 +1,31 @@
+import { useState, useLayoutEffect } from 'react'
 import './style.scss'
 import { Card } from '../Card'
 
-export function Main(props) {
-    const championsKeys = Object.keys(props.champions)
+export function Main() {
+    const [championsList, setChampionsList] = useState({name: '', title: ''});
+    const[finallyState, setFinallyState] = useState(true);
+    const championsKeys = Object.keys(championsList)
+  
+    useLayoutEffect(() => {
+        async function getApiAsync() {
+            const response = await fetch("http://ddragon.leagueoflegends.com/cdn/12.23.1/data/pt_BR/champion.json");
+            const data = await response.json();
+  
+            setChampionsList(Object.assign({}, data.data))
+        }
+  
+        getApiAsync()
+          .catch(error => console.error('Erro ao carregar a API', error))
+          .finally(() => setFinallyState(false));
+    }, [])
+  
+    if(finallyState) return ('loading...');
     
     return(
         <main>
             {
-                championsKeys.map(el => <Card name={props.champions[el].name} title={props.champions[el].title} img={props.champions[el].image.full} key={championsKeys.indexOf(el)}/>)
+                championsKeys.map(el => <Card name={championsList[el].name} title={championsList[el].title} img={championsList[el].image.full} key={championsKeys.indexOf(el)}/>)
             }
         </main>
     )
